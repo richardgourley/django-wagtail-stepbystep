dcc186cd08059d09cb007efee10e88e19032453e
Rich <codeleap@outlook.com>
2020-11-04 14:40:29 +0100

    Compress precompilers added to settings

--- projectfolder/mysite/mysite/settings/base.py
+++ projectfolder/mysite/mysite/settings/base.py
@@ -170,3 +170,7 @@ WAGTAIL_SITE_NAME = "mysite"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
