44733168cfa377a91faa1e37632474a148ac490e
Rich <codeleap@outlook.com>
2020-11-06 18:56:29 +0100

    Model errors with ForeignKey fixed.

--- projectfolder/mysite/surgeries/models.py
+++ projectfolder/mysite/surgeries/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class MedicalSpecialization(models.Model):
-	name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
 class City(models.Model):
-	name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
 class Doctor(models.Model):
-	first_name = models.CharField(max_length=255)
-	surname = models.CharField(max_length=255)
-	specializations = models.ManyToManyField(Specializations, help_text='Select 1 or more specializations.')
+    first_name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    specializations = models.ManyToManyField(MedicalSpecialization, help_text='Select 1 or more specializations.')
 
 class Surgery(models.Model):
-	surgery_name = models.CharField(max_length=255)
-	address = models.TextField()
-    city = ForeignKey(City, on_delete=models.SET_NULL)
+    surgery_name = models.CharField(max_length=255)
+    address = models.TextField()
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
     doctors = models.ManyToManyField(Doctor, help_text='Select which doctors are based at this surgery.')
