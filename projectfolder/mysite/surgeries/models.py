7f1813aff5cfe721e80bdb3d799d04c7710a6e83
Rich <codeleap@outlook.com>
2020-11-06 18:23:10 +0100

    Models first draft added.

--- projectfolder/mysite/surgeries/models.py
+++ projectfolder/mysite/surgeries/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MedicalSpecialization(models.Model):
+	name = models.CharField(max_length=255)
+
+class City(models.Model):
+	name = models.CharField(max_length=255)
+
+class Surgery(models.Model):
+	surgery_name = models.CharField(max_length=255)
+	address = models.TextField()
+    city = ForeignKey(City, on_delete=models.SET_NULL)
