bf3ed1518958186b4cbe2d17cee108164a6cc02f
Rich <codeleap@outlook.com>
2020-11-06 18:49:16 +0100

    Doctor model added.

--- projectfolder/mysite/surgeries/models.py
+++ projectfolder/mysite/surgeries/models.py
@@ -7,7 +7,13 @@ class MedicalSpecialization(models.Model):
 class City(models.Model):
 	name = models.CharField(max_length=255)
 
+class Doctor(models.Model):
+	first_name = models.CharField(max_length=255)
+	surname = models.CharField(max_length=255)
+	specializations = models.ManyToManyField(Specializations, help_text='Select 1 or more specializations.')
+
 class Surgery(models.Model):
 	surgery_name = models.CharField(max_length=255)
 	address = models.TextField()
     city = ForeignKey(City, on_delete=models.SET_NULL)
+    doctors = models.ManyToManyField(Doctor, help_text='Select which doctors are based at this surgery.')
