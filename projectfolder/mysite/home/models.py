36cfcd255a2fee9894a9557ea019ca371ca24885
Rich <codeleap@outlook.com>
2020-11-17 21:01:40 +0100

    Intro and main image added to HomePage model.

--- projectfolder/mysite/home/models.py
+++ projectfolder/mysite/home/models.py
@@ -5,4 +5,12 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    intro = models.CharField(blank=False, null=True, max_length=255)
+    main_image = models.ForeignKey(
+    	'wagtailimages.Image',
+    	 null=True, 
+    	 blank=True, 
+         on_delete=models.SET_NULL, 
+         related_name="+", 
+         help_text='This image will appear in a full width image behind your intro text.'
+    )
