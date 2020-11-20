b80430215e16bc91a2ddde76dabb20eea5403020
Rich <codeleap@outlook.com>
2020-11-20 14:26:44 +0100

    Order of imports changed.

--- projectfolder/mysite/surgeries/models.py
+++ projectfolder/mysite/surgeries/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
 class SurgeryIndexPage(Page):
