ddea7c5b5e2cb2a8a9103996a47b1ffc18898cd7
Rich <codeleap@outlook.com>
2020-11-20 14:27:14 +0100

    Blank = true removed from home page image.

--- projectfolder/mysite/home/models.py
+++ projectfolder/mysite/home/models.py
@@ -11,8 +11,7 @@ class HomePage(Page):
     intro = models.CharField(blank=False, null=True, max_length=255)
     main_image = models.ForeignKey(
     	'wagtailimages.Image',
-    	 null=True, 
-    	 blank=True, 
+    	 null=True,
          on_delete=models.SET_NULL, 
          related_name="+", 
          help_text='This image will appear in a full width image behind your intro text.'
