cd42a73cbf5a3b4d3fde33e8a7a8d0de9046fc7f
Rich <codeleap@outlook.com>
2020-11-24 16:08:22 +0100

    DoctorDetailView added.

--- projectfolder/mysite/surgeries/views.py
+++ projectfolder/mysite/surgeries/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Doctor
 
 # Create your views here.
+class DoctorDetailView(generic.DetailView):
+	model = Doctor
+	template_name = 'surgeries/doctor_detail.html'
+
+	def get_queryset(self):
+		return Doctor.objects.all()
