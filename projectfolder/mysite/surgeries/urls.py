f1158db055ea71333ef135274915f3c7e9a94ca4
Rich <codeleap@outlook.com>
2020-11-24 16:07:22 +0100

    DoctorDetailView url added.

--- projectfolder/mysite/surgeries/urls.py
+++ projectfolder/mysite/surgeries/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'surgeries'
 
-urlpatters = []
+urlpatterns = [
+    path('doctor/<slug:slug>', views.DoctorDetailView.as_view(), name="doctor_detail"),
+]
